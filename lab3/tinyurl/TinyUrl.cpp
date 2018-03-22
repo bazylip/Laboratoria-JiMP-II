77520d6cf86b09b0143a64a2087ba93e8aae9358
BazyliP <bazyli.polednia@gmail.com>
2018-03-22 23:25:33 +0100

    TinyUrl

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "TinyUrl.h"
+
+namespace tinyurl {
+
+    std::unique_ptr<TinyUrlCodec> Init(){
+        auto codec = std::make_unique<TinyUrlCodec>();
+        codec->PreviousCodec = {'0','0','0','0','0','0'};
+
+        return codec;
+    }
+
+    void NextHash(std::array<char, 6> *state) {
+        using ::std::array;
+        int i = 5;
+
+        while (i >= 0) {
+
+
+            if ((*state)[i] != 57 and (*state)[i] != 90 and (*state)[i] != 122) {
+                (*state)[i] += 1;
+                break;
+            } else if ((*state)[i] == 57) {
+                (*state)[i] = 65;
+                break;
+            } else if ((*state)[i] == 90) {
+                (*state)[i] = 97;
+                break;
+            } else {
+                (*state)[i] = 48;
+                --i;
+            }
+        }
+    };
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec){
+        (*codec)->map.emplace(url, (*codec)->PreviousCodec);
+        std::string output;
+        for(auto c : (*codec)->PreviousCodec){
+            output += c;
+        }
+        NextHash(& (*codec)->PreviousCodec);
+
+        return output;
+    }
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash){
+        std::array<char, 6> NewHash;
+        int i = 0;
+        for(char c : hash){
+            NewHash[i] = c;
+            ++i;
+        }
+        for(auto element : codec->map){
+            if(element.second == NewHash){
+                return element.first;
+            }
+        }
+    }
+
+}
--- lab3/tinyurl/TinyUrl.h
+++ lab3/tinyurl/TinyUrl.h
@@ -8,15 +8,24 @@
 #include <string>
 #include <array>
 #include <memory>
+#include "map"
+#include "string"
 
-struct TinyUrlCodec{
 
-};
 
-std::unique_ptr<TinyUrlCodec> Init();
-void NextHash(std::array<char, 6> *state);
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+namespace tinyurl {
+    struct TinyUrlCodec{
+        std::array<char, 6> PreviousCodec;
+        std::map<std::string, std::array<char, 6> > map;
+    };
 
+    std::unique_ptr<TinyUrlCodec> Init();
+
+    void NextHash(std::array<char, 6> *state);
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+}
 
 #endif //JIMP_EXERCISES_TINYURL_H
