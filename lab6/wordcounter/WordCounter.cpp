b325093148abd0b74912b9debac311c8e6ea3f56
BazyliP <bazyli.polednia@gmail.com>
2018-04-20 01:35:28 +0200

    Word Counter

--- lab6/wordcounter/WordCounter.cpp
+++ lab6/wordcounter/WordCounter.cpp
@@ -3,4 +3,83 @@
 //
 
 #include "WordCounter.h"
+#include "iostream"
 
+namespace datastructures {
+    Counts &Counts::operator++(int i) {
+        count_++;
+        return *this;
+    }
+
+//    bool Counts::operator==(const Counts &another) {
+//        return (count_ == another.GetCount());
+//    }
+
+    bool operator==(const Counts &another, const int number){
+        return(another.GetCount() == number);
+    }
+
+    bool Counts::operator<(const Counts &another) {
+        return (count_ < another.GetCount());
+    }
+
+    bool Counts::operator>(const Counts &another) {
+        return (count_ > another.GetCount());
+    }
+
+    const bool operator<(const Word &first, const Word &other) {
+        return (first.GetWord() < other.GetWord());
+    }
+    const bool operator==(const Word &first, const Word &other) {
+        return (first.GetWord() == other.GetWord());
+    }
+
+//    WordCounter::WordCounter(const std::initializer_list<std::string> &StringList) {
+//        for (auto word : StringList) {
+//            bool change = false;
+//            for (auto elem : dict_) {
+//                if (word == elem.first.GetWord()) {
+//                    elem.second++;
+//                    change = true;
+//                }
+//            }
+//            if (!change) {
+//                dict_.emplace_back(std::make_pair(Word(word), 0));
+//            }
+//        }
+//    }
+
+    int WordCounter::operator[](const std::string &str) {
+        for (auto elem : dict_) {
+            if (str == elem.first.GetWord()) {
+                int amount = elem.second.GetCount();
+                return amount;
+            }
+        }
+
+        return 0;
+    }
+
+    int WordCounter::DistinctWords() {
+        std::cout<<"wielkosc: "<<dict_.size()<<" "<<std::endl;
+        return dict_.size();
+    }
+
+    int WordCounter::TotalWords() {
+        int result = 0;
+        for (auto el : dict_) {
+            result += el.second.GetCount();
+        }
+
+        return result;
+    }
+
+    std::set<Word> WordCounter::Words() {
+        std::set<Word> result;
+        for (auto el : dict_) {
+            result.emplace(el.first.GetWord());
+        }
+
+        return result;
+    }
+}
--- lab6/wordcounter/WordCounter.h
+++ lab6/wordcounter/WordCounter.h
@@ -6,47 +6,87 @@
 #define JIMP_EXERCISES_WORDCOUNTER_H
 
 #include "string"
-#include "set"
+#include "vector"
 #include "initializer_list"
+#include "set"
 
-class Counts{
-public:
-    Counts();
-    Counts(int i);
-    Counts &operator<(Counts &another);
-    Counts &operator>(Counts &another);
-    Counts &operator==(Counts &another);
-    Counts &operator++(int i);
+namespace datastructures {
+    class Counts {
+    public:
+        Counts() : count_{1} {}
 
+        Counts(int i) : count_{i} {}
 
-private:
-    int count_;
-};
+        inline void Increment(){
+            count_++;
+        }
 
-class Word{
-public:
-    Word();
-    Word(const std::string &str);
+        friend bool operator==(const Counts &another, const int number);
 
-private:
-    std::string word_;
-};
+        bool operator<(const Counts &another);
 
-class WordCounter{
-public:
-    WordCounter();
-    WordCounter(const std::initializer_list<std::string> &StringList);
-    WordCounter &operator[](const std::string &str);
-    WordCounter &operator<<(const std::string &file);
+        bool operator>(const Counts &another);
 
-    int DistinctWords();
-    int TotalWords();
-    std::set<Word> Words();
+        Counts &operator++(int i);
+
+        inline int GetCount() const { return count_; }
+
+    private:
+        int count_;
+    };
 
 
-private:
-    std::set<std::pair<Word, Counts> > dict_;
-};
 
+    class Word {
+    public:
+        Word() : word_{""} {}
+
+        Word(const std::string &str) : word_{str} {}
+
+        friend const bool operator<(const Word &first, const Word &other);
+        friend const bool operator==(const Word &first, const Word &other);
+        inline std::string GetWord() const { return word_; }
+
+    private:
+        std::string word_;
+    };
+
+    const bool operator<(const Word &first, const Word &other);
+    const bool operator==(const Word &first, const Word &other);
+
+    class WordCounter {
+    public:
+        WordCounter() {}
+
+        WordCounter(const std::initializer_list<Word> &List) : vector_{List}{
+            for (auto word : vector_) {
+                bool change = false;
+                for (auto &elem : dict_) {
+                    if (word.GetWord() == elem.first.GetWord()) {
+                        elem.second.Increment();
+                        change = true;
+                    }
+                }
+                if (!change) {
+                    dict_.emplace_back(std::make_pair(Word(word), 1));
+                }
+            }
+        }
+
+        int operator[](const std::string &str);
+//    WordCounter operator<<(const std::string &file);
+
+
+        int DistinctWords();
+
+        int TotalWords();
+
+        std::set<Word> Words();
+
+    private:
+        std::vector<std::pair<Word, Counts> > dict_;
+        std::vector<Word> vector_;
+    };
+}
 
 #endif //JIMP_EXERCISES_WORDCOUNTER_H
