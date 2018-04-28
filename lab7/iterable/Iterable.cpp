ac27bb7bb760f228a77015675f08881c6c618473
BazyliP <bazyli.polednia@gmail.com>
2018-04-28 12:55:26 +0200

    Iterable

--- lab7/iterable/Iterable.cpp
+++ lab7/iterable/Iterable.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Iterable.h"
+
+namespace utility{
+    std::pair<int, std::string> ZipperIterator::Dereference() const {
+        return std::pair <int, std::string> (*LeftIterator_, *RightIterator_);
+    }
+
+    IterableIterator& ZipperIterator::Next() {
+        if(LeftIterator_ != LeftEnd_)
+            ++LeftIterator_;
+        if(RightIterator_ != RightEnd_)
+            ++RightIterator_;
+
+        return *this;
+    }
+
+    std::vector<int>::const_iterator& ZipperIterator::GetLeft() {
+        return LeftIterator_;
+    }
+
+    std::vector<std::string>::const_iterator& ZipperIterator::GetRight() {
+        return RightIterator_;
+    }
+//
+//    bool ZipperIterator::NotEquals(const std::unique_ptr<utility::IterableIterator> &other) const {
+//        return(this == other)
+//    }
+//
+//    bool IterableIteratorWrapper::operator!=(const IterableIteratorWrapper &other) const {
+//        return NotEquals(std::unique_ptr<IterableIteratorWrapper> );
+//    }git status
+//
+//    std::pair<int, std::string> IterableIteratorWrapper::Dereference() const {
+//        return Iterator_->Dereference();
+//    }
+//
+//    IterableIterator& IterableIteratorWrapper::Next() {
+//        return Iterator_->Next();
+//    }
+}
--- lab7/iterable/Iterable.h
+++ lab7/iterable/Iterable.h
@@ -7,6 +7,7 @@
 
 #include "string"
 #include "memory"
+#include "vector"
 
 namespace utility{
 
@@ -15,7 +16,7 @@ namespace utility{
         virtual ~IterableIterator() = default;
         virtual std::pair<int, std::string> Dereference() const =0;
         virtual IterableIterator &Next() =0;
-        virtual bool NotEquals(const std::unique_ptr<utility::IterableIterator>&) const =0;
+        virtual bool NotEquals(const std::unique_ptr<utility::IterableIterator> &other) const =0;
     };
 
     class ZipperIterator : public IterableIterator{
@@ -25,9 +26,32 @@ namespace utility{
                                 std::vector<int>::const_iterator left_end,
                                 std::vector<std::string>::const_iterator right_end);
 
+        ~ZipperIterator() override = default;
+        std::pair<int, std::string> Dereference() const override;
+        IterableIterator &Next() override;
+        bool NotEquals(const std::unique_ptr<utility::IterableIterator> &other) const override;
+        std::vector<int>::const_iterator &GetLeft();
+        std::vector<std::string>::const_iterator &GetRight();
 
+    private:
+        std::vector<int>::const_iterator LeftIterator_;
+        std::vector<std::string>::const_iterator RightIterator_;
+        std::vector<int>::const_iterator LeftEnd_;
+        std::vector<std::string>::const_iterator RightEnd_;
     };
 
+    class Zipper{};
+
+//    class IterableIteratorWrapper : public IterableIterator{
+//    public:
+//        IterableIteratorWrapper(std::unique_ptr<IterableIterator> iterator): Iterator_{iterator}{}
+//        ~IterableIteratorWrapper() override = default;
+//        bool operator!=(const IterableIteratorWrapper &other) const;
+//        std::pair<int, std::string> operator*() const;
+//        IterableIteratorWrapper &operator++();
+//
+//        std::unique_ptr<utility::IterableIterator> Iterator_;
+//    };
 
 
 }
